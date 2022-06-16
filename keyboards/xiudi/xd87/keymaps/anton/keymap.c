9f986a53aee3dcdbd16ea78ee4a8461b6115d5ea
Anton Mosich <anton@mosich.at>
2022-06-16 12:09:31 +0200

    Change caps lock key to esc/altgr

--- keyboards/xiudi/xd87/keymaps/anton/keymap.c
+++ keyboards/xiudi/xd87/keymaps/anton/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS, \
       KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
       KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,            KC_DEL,  KC_END,  KC_PGDN, \
-      KC_BSPC,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS, KC_ENT, \
+      RALT_T(KC_ESC), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS, KC_ENT, \
       KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP, \
       KC_LCTL,KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,MO(1), KC_APP,  KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  ),
 
@@ -69,7 +69,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case TABLE:
       if(record->event.pressed) {
-//        send_unicode_hex_string("0028 FF89 0CA5 76CA 0CA5 FF09 FF89 5F61 253B 2501 253B");
         send_unicode_string("(ﾉಥ益ಥ）ﾉ彡┻━┻");
       } else {
 
@@ -78,7 +77,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case LENNY:
       if(record->event.pressed) {
-//        send_unicode_hex_string("0028 0020 0361 00B0 0020 035C 0296 0020 0361 00B0 0029 000A");
         send_unicode_string("( ͡° ͜ʖ ͡°)");
       } else {
 
@@ -87,7 +85,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case SHRUG:
       if(record->event.pressed) {
-//        send_unicode_hex_string("00AF 005C 005F 0028 30C4 0029 005F 002F 00AF 000A");
         send_unicode_string("¯\\(ツ)/¯");
       } else {
 
@@ -96,7 +93,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     case SHRUG2:
       if(record->event.pressed) {
-//        send_unicode_hex_string("00AF 005C 005C 005C 005F 0028 30C4 0029 005C 005F 002F 00AF 000A");
         send_unicode_string("¯\\\\(ツ)/¯");
       } else {
 
